1d5d3c55b7f5d526f36ee76f5aa8fee6b66808e7
Ligoml <39876205+Ligoml@users.noreply.github.com>
2022-01-06 12:12:52 +0800

    Add files via upload

---
+++ checkpoint_2/yolo_paddle/run_eval.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH=.
+
+python metric_eval.py  \
+    --dataset_dir /home/bml/.storage/mnt/v-uimvattx3skxxwr7/org/workflow/VOCdevkit/ \
+    --trained_model ../../weights_trans/yolo_paddle.pdparams \
+    --gpu
